fb757caf3eba1303fff51f33636f5f79e1948bf1
kakgonbri <buikhacbin@gmail.com>
2025-05-29 16:06:07 +0700

    fixed naming

--- src/test/java/InsuranceClaimTest.java
+++ src/test/java/InsuranceClaimTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
-class InsuranceClaimTest {
+public class InsuranceClaimTest {
 
     private InsuranceClaim claim;
 
